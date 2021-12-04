fb616cfd145ee9410e6b1982bb2e271af47feff5
mkamergani <mkmergani@gmail.com>
2021-12-05 00:30:30 +0200

    create new template, action and route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -13,6 +13,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+
+  # config.reload_classes_only_on_change = false # added this so server updates automaticaly without restart
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
